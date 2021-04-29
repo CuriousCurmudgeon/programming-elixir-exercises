a927d9d9c33b5eda94014fe327edbc8b1a230744
Brian Meeker <meeker.brian@gmail.com>
2021-04-29 02:30:06 +0000

    Made helper functions private

--- chapter-6/chop.exs
+++ chapter-6/chop.exs
@@ -8,19 +8,19 @@ defmodule Chop do
         IO.puts "Guess must be in the range"
     end
 
-    def guess_helper(actual, _lower.._upper, current) when actual == current do
+    defp guess_helper(actual, _lower.._upper, current) when actual == current do
         IO.puts current
     end
 
-    def guess_helper(actual, _lower..upper, current) when actual > current do
+    defp guess_helper(actual, _lower..upper, current) when actual > current do
         print_is_it current
         guess_helper(actual, current..upper, div(upper - current, 2) + current)
     end
 
-    def guess_helper(actual, lower.._upper, current) when actual < current do
+    defp guess_helper(actual, lower.._upper, current) when actual < current do
         print_is_it current
         guess_helper(actual, lower..current, div(current - lower, 2) + lower)
     end
 
-    def print_is_it(current), do: IO.puts "Is it #{current}"
+    defp print_is_it(current), do: IO.puts "Is it #{current}"
 end
