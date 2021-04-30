b76d6859ac1932cda0406b27a497be29cdb3d4a5
root <root@CustomPC.localdomain>
2021-04-29 22:53:06 -0400

    Implemented exercise ListsAndRecursion-1 in chapter 7

--- chapter-7/math.exs
+++ chapter-7/math.exs
@@ -1,5 +1,14 @@
 defmodule Math do
   # Exercise: ListsAndRecursion-0
-  def sum([]), do: 0
-  def sum([head | tail]), do: sum(tail) + head
+  def sum([]),              do: 0
+  def sum([ head | tail ]), do: sum(tail) + head
+
+  # Exercise: ListsAndRecursion-1
+  def mapsum(list, func) do
+    map(list, func)
+    |> sum
+  end
+
+  def map([], _func),             do: []
+  def map([ head | tail ], func), do: [ func.(head) | map(tail, func) ]
 end
