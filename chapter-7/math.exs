941eb06b5ed120712ba692a6c498897c7ca48d2b
Brian Meeker <meeker.brian@gmail.com>
2021-05-04 22:24:10 -0400

    Implemented exercise ListsAndRecursion-2

--- chapter-7/math.exs
+++ chapter-7/math.exs
@@ -11,4 +11,11 @@ defmodule Math do
 
   def map([], _func),             do: []
   def map([ head | tail ], func), do: [ func.(head) | map(tail, func) ]
+
+  # Exercise: ListsAndRecursion-2
+  def max([ head | tail ]), do: _max(tail, head)
+
+  defp _max([], current_max), do: current_max
+  defp _max([ head | tail ], current_max) when head > current_max, do: _max(tail, head)
+  defp _max([ head | tail ], current_max) when head <= current_max, do: _max(tail, current_max)
 end
