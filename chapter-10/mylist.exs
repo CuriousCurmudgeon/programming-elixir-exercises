5a4c78404c7ed006e81de07f5144d7817100641d
Brian Meeker <meeker.brian@gmail.com>
2021-05-09 22:16:30 -0400

    Implemented exercise ListsAndRecursion-8
    I included a commented out version of the data. I'm being pretty sloppy with how I'm going back and forth between *.exs files and iex, but I'm fine with it for learning.

--- chapter-10/mylist.exs
+++ chapter-10/mylist.exs
@@ -38,4 +38,28 @@ defmodule MyList do
   def primes(n) when n >= 2 do
     for x <- Math.span(2,n), Enum.all?(Math.span(2,x-1), &(rem(x,&1)!=0)), do: x
   end
+
+  # Exercise: ListsAndRecursion-8
+  # orders = [
+  #   [id: 123, ship_to: :NC, net_amount: 100.00],
+  #   [id: 124, ship_to: :OK, net_amount:  35.50],
+  #   [id: 125, ship_to: :TX, net_amount:  24.00],
+  #   [id: 126, ship_to: :TX, net_amount:  44.80],
+  #   [id: 127, ship_to: :NC, net_amount:  25.00],
+  #   [id: 128, ship_to: :MA, net_amount:  10.00],
+  #   [id: 129, ship_to: :CA, net_amount: 102.00],
+  #   [id: 130, ship_to: :NC, net_amount:  50.00],
+  # ]
+  # tax_rates = [NC: 0.075, TX: 0.08]
+  def calculate_total(orders, tax_rates) do
+    # for order <- orders, tax_rate <- tax_rates, Keyword.has_key?(tax_rates, order[:ship_to]), do: Keyword.put(order, :total_amount, order[:net_amount] * tax_rates[order[:ship_to]])
+    for order <- orders do
+      net_amount = order[:net_amount]
+      ship_to = order[:ship_to]
+
+      tax_rate = Keyword.get(tax_rates, ship_to, 0)
+
+      order ++ [total_amount: net_amount + (net_amount * tax_rate)]
+    end
+  end
 end
