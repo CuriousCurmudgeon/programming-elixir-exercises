065d3b46fb51b84afc2841f5a40ea12c203d0e52
Brian Meeker <meeker.brian@gmail.com>
2021-05-21 22:47:44 -0400

    Added the provided implementation of number to help out with the calculate exercise

--- chapter-11/mystrings.exs
+++ chapter-11/mystrings.exs
@@ -6,4 +6,24 @@ defmodule MyStrings do
 
     # Exercise: StringsAndBinaries-2
     def anagram?(word1, word2), do: Enum.sort(word1) == Enum.sort(word2)
+
+    # Exercise: StringsAndBinaries-4
+    # This breaks down to number [+-*/] number.
+    # So, start by reusing number, except the terminal case is space.
+    # Then, look for an operator
+    # Then another space
+    # Then another number.
+
+    def number([ ?- | tail ]), do: _number_digits(tail, 0) * -1
+    def number([ ?+ | tail ]), do: _number_digits(tail, 0)
+    def number(str),           do: _number_digits(str, 0)
+
+    defp _number_digits([], value), do: value
+    defp _number_digits([ digit | tail ], value)
+    when digit in '0123456789' do
+        _number_digits(tail, value * 10 + digit - ?0)
+    end
+    defp _number_digits([ non_digit | _ ], _) do
+        raise "Invalid digit '#{[non_digit]}'"
+    end
 end
