af4fe33ffe14b81d00b3c557b7fc15b9cfc0199e
Brian Meeker <meeker.brian@gmail.com>
2021-05-29 21:24:56 -0400

    Not quite formatting the results correctly.
    I've been staring at this too long. I'll come back with fresh eyes later to find the mistake.

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -7,7 +7,7 @@ defmodule Issues.CLI do
   table of of the last _n_ issues in a github project.
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -56,6 +56,7 @@ defmodule Issues.CLI do
     |> decode_response
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> Issues.TableFormatter.print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
